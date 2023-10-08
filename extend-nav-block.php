6a5f43acdf2f96b04691cea55a5b4292f8469c1c
Tanvirul Haque <tanvirulhaque123@gmail.com>
2023-10-08 18:03:44 +0600

    Add option

--- extend-nav-block.php
+++ extend-nav-block.php
@@ -9,17 +9,24 @@ Version:        1.0.0
 Text Domain:    extend-nav-block
 */
 
-if (! defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action('enqueue_block_editor_assets', function() {
-	$config = require_once plugin_dir_path(__FILE__) . '/build/index.asset.php';
+add_action( 'enqueue_block_editor_assets', function () {
+	$config = require_once plugin_dir_path( __FILE__ ) . '/build/index.asset.php';
+
+	wp_enqueue_style(
+		'extend-nav-block-style',
+		plugin_dir_url( __FILE__ ) . '/build/index.css',
+		array(),
+		$config['version']
+	);
 
 	wp_enqueue_script(
 		'extend-nav-block-script',
-		plugin_dir_url(__FILE__) . '/build/index.js',
+		plugin_dir_url( __FILE__ ) . '/build/index.js',
 		$config['dependencies'],
 		$config['version']
 	);
-});
+} );
--- src/index.js
+++ src/index.js
@@ -1,26 +1,86 @@
-import { addFilter } from '@wordpress/hooks';
-import { createHigherOrderComponent } from '@wordpress/compose';
-import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, RangeControl, __experimentalUnitControl as UnitControl } from '@wordpress/components';
+import {addFilter} from '@wordpress/hooks';
+import {createHigherOrderComponent} from '@wordpress/compose';
+import {InspectorControls} from '@wordpress/block-editor';
+import {PanelBody, RangeControl, __experimentalUnitControl as UnitControl} from '@wordpress/components';
 
-const withWrapperForMegaMenu = createHigherOrderComponent( ( BlockEdit ) => {
-    return ( props ) => {
+import { BlockControls} from '@wordpress/block-editor';
+import {Toolbar, ToolbarGroup, ToolbarButton} from '@wordpress/components';
+
+import { grid } from '@wordpress/icons';
+
+import './index.scss';
+
+// Adding wrapper div in backend
+// const withWrapperForMegaMenu = createHigherOrderComponent( ( BlockEdit ) => {
+//     return ( props ) => {
+//         if (props.name !== "core/navigation-submenu") {
+//             return <BlockEdit { ...props } />;
+//         }
+//
+//         return (
+//             <>
+//                 <div className="mega-menu-wrapper">
+//                     <BlockEdit { ...props } />
+//                 </div>
+//             </>
+//         );
+//     };
+// }, "withWrapperForMegaMenu" );
+//
+// addFilter(
+//     'editor.BlockEdit',
+//     'extend-nav-block/edit',
+//     withWrapperForMegaMenu
+// );
+
+
+// Adding wrapper div in saved (after reloading saved block)
+// function wrapSubNavigationBlockInContainer( element, blockType, attributes ) {
+//     if ( ! element ) {
+//         return;
+//     }
+//
+//     if ( blockType.name !== 'core/navigation-submenu' ) {
+//         return element;
+//     }
+//
+//     // return the element wrapped in a div
+//     return <div className="mega-menu-wrapper">{ element }</div>;
+// }
+//
+// wp.hooks.addFilter(
+//     'blocks.getSaveElement',
+//     'extend-nav-block/wrap-sub-navigation-in-container',
+//     wrapSubNavigationBlockInContainer
+// );
+
+// Adding mega menu blockcontrol
+const addSubNavMegaMenuOption = createHigherOrderComponent((BlockEdit) => {
+    return (props) => {
         if (props.name !== "core/navigation-submenu") {
-            return <BlockEdit { ...props } />;
+            return <BlockEdit {...props} />;
         }
+
+        const {attributes, setAttributes} = props;
 
         return (
             <>
-                <div className="mega-menu-wrapper">
-                    <BlockEdit { ...props } />
-                </div>
+                <BlockEdit {...props} />
+
+                <BlockControls group="block">
+                    <ToolbarButton
+                        icon={ grid }
+                        label="Mega Menu"
+                        onClick={() => alert('Editing')}
+                    />
+                </BlockControls>
             </>
         );
     };
-}, "withWrapperForMegaMenu" );
+}, "addSubNavMegaMenuOption");
 
 addFilter(
     'editor.BlockEdit',
     'extend-nav-block/edit',
-    withWrapperForMegaMenu
+    addSubNavMegaMenuOption
 );
